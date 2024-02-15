d321c945ce4802d34e61fe23226fbc80136bc8d0
Ben Makusha <btmakusha@gmail.com>
2024-02-14 23:27:50 -0500

    Add Create User Function from google cloud

--- yt-web-client/yt-clone-app/app/utilities/firebase/firebase.ts
+++ yt-web-client/yt-clone-app/app/utilities/firebase/firebase.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth,
         signInWithPopup,
         GoogleAuthProvider,
@@ -23,7 +22,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 const auth = getAuth(app)
 
